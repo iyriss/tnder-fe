dc3dc6121300c80e6e3e822b7db4f25ee34420b8
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2022-03-31 12:19:48 -0700

    added authentication

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,3 +1,18 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
 export const Home: React.FC = () => {
-  return <div>Home</div>;
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <div>Not logged in</div>;
+  }
+
+  return isAuthenticated && <div>Home</div>;
 };
+
+export default Home;
