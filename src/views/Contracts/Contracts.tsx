3ef7557badc6366235e4019564a1de84818a4857
Omar Hernandez <comhernandez28@gmail.com>
2022-03-31 14:02:48 -0700

    updated contracts

--- src/views/Contracts/Contracts.tsx
+++ src/views/Contracts/Contracts.tsx
@@ -1,10 +1,37 @@
 import React from "react";
-import "./contracts.css";
+import styled from "styled-components";
 
-export const Contracts: React.FC = () => {
+const userProfiles = [
+  { name: "test", age: 29 },
+  { name: "test 2", age: 22 },
+  { name: "test 3", age: 37 },
+];
+
+const Container = styled.div`
+  border: 2px solid black;
+  display: flex;
+`;
+
+const ContractsProfile = (props: any) => {
   return (
     <div>
-      <div>Contracts</div>
+      <div>{props.user.name}</div>
+      <div>{props.user.age}</div>
     </div>
   );
 };
+
+export const Contracts: React.FC = () => {
+  return (
+    <Container>
+      <div>Contracts</div>
+      <div>
+        {userProfiles.map(function (user, i) {
+          return <ContractsProfile user={user} key={i} />;
+        })}
+      </div>
+    </Container>
+  );
+};
+
+export default Contracts;
