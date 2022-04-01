b5134c44f1f5651503bfd2d7baae0c22ab02e32b
iyriss <iyrisvigil@gmail.com>
2022-04-01 10:56:28 -0700

    add carousel

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
-import { GazeCloudProvider } from "./providers/gazeCloud";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { GazeCloudProvider } from './providers/gazeCloud';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-y3jjd8l2.us.auth0.com"
-      clientId="2i8KFZYhYov1Ih4rUcisknpeq4UdMp7a"
+      domain='dev-y3jjd8l2.us.auth0.com'
+      clientId='2i8KFZYhYov1Ih4rUcisknpeq4UdMp7a'
       redirectUri={window.location.origin}
     >
       <GazeCloudProvider>
@@ -24,5 +25,5 @@ ReactDOM.render(
       </GazeCloudProvider>
     </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
