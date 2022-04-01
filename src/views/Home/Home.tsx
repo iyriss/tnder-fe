f6a1c1c2c3f8991ce800cedfdab0730219cc1d6c
Omar Hernandez <comhernandez28@gmail.com>
2022-04-01 12:20:17 -0700

    added basic matching

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import {
   Carousel,
   CarouselItem,
   CarouselControl,
   CarouselIndicators,
-} from 'reactstrap';
+} from "reactstrap";
 
-import * as S from './Home.styled';
-import DislikeIcon from '../../components/icons/DislikeIcon';
-import LikeIcon from '../../components/icons/LikeIcon';
-import SpyIcon from '../../components/icons/SpyIcon';
-import { useGazeProvider } from '../../providers/gazeCloud';
-import { MissionProfile } from './MissionProfiles';
-import { MissionsCompleted } from './MissionsCompleted/MissionsCompleted';
-import format_data, { BubbleData } from '../../utils/FormatData';
-import { ProfileApi } from '../../apis/ProfileApi';
-import './MissionProfiles.css';
+import * as S from "./Home.styled";
+import DislikeIcon from "../../components/icons/DislikeIcon";
+import LikeIcon from "../../components/icons/LikeIcon";
+import SpyIcon from "../../components/icons/SpyIcon";
+import { useGazeProvider } from "../../providers/gazeCloud";
+import { MissionProfile } from "./MissionProfiles";
+import { MissionsCompleted } from "./MissionsCompleted/MissionsCompleted";
+import format_data, { BubbleData } from "../../utils/FormatData";
+import { ProfileApi } from "../../apis/ProfileApi";
+import "./MissionProfiles.css";
 
 export const Home: React.FC = () => {
   const [formattedData, setFormattedData] = useState<Array<BubbleData>>([]);
@@ -29,27 +29,10 @@ export const Home: React.FC = () => {
     { data, error, isProcessing },
   ] = useGazeProvider();
 
-  const [profiles, setProfiles] = useState([
-    {
-      avatar:
-        'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png',
-      name: 'Luis',
-      age: 29,
-      job: 'MI6 @ British Intelligence ',
-      bio: 'Experienced infiltration operator. Count on me to get the job done. Quietly.',
-    },
-    {
-      avatar:
-        'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png',
-      name: 'Luis',
-      age: 22,
-      job: 'MI6 @ British Intelligence ',
-      bio: 'Experienced infiltration operator. Count on me to get the job done. Quietly.',
-    },
-  ]);
+  const [profiles, setProfiles] = useState([{}]);
 
   useEffect(() => {
-    console.log('user: ', user);
+    console.log("user: ", user);
     //cant use async in useffect
     const getData = async () => {
       const profiles = await profileApi.getProfiles(user?.email);
@@ -77,11 +60,11 @@ export const Home: React.FC = () => {
   }, [data]);
 
   useEffect(() => {
-    console.log('error: ', error);
+    console.log("error: ", error);
   }, [error]);
 
   useEffect(() => {
-    console.log('isProcessing: ', isProcessing);
+    console.log("isProcessing: ", isProcessing);
   }, [isProcessing]);
 
   useEffect(() => {
@@ -123,10 +106,23 @@ export const Home: React.FC = () => {
   if (missionsCleared) {
     return <MissionsCompleted />;
   }
+
+  const handleLike = () => {
+    profileApi.likeUser({
+      user: user?.email,
+      //@ts-ignore
+      likedUser: profiles[activeIndex].user,
+    });
+    console.log("liked");
+  };
+  const handleDislike = () => {
+    console.log("dislied");
+  };
+
   return (
     <S.DashboardPage>
       <S.Title>
-        Welcome back USERNAME!{' '}
+        Welcome back USERNAME!{" "}
         {profiles.length < 2 ? (
           <span>You have 1 mission available today.</span>
         ) : (
@@ -135,7 +131,7 @@ export const Home: React.FC = () => {
           </span>
         )}
       </S.Title>
-      <div style={{ width: '100vw' }}>
+      <div style={{ width: "100vw" }}>
         <Carousel
           interval={false}
           activeIndex={activeIndex}
@@ -157,14 +153,14 @@ export const Home: React.FC = () => {
                 {MissionProfile(profile)}
                 {activeIndex !== 0 ? (
                   <CarouselControl
-                    direction='prev'
-                    directionText='Previous'
+                    direction="prev"
+                    directionText="Previous"
                     onClickHandler={previous}
                   />
                 ) : null}
                 <CarouselControl
-                  direction='next'
-                  directionText='Next'
+                  direction="next"
+                  directionText="Next"
                   onClickHandler={next}
                 />
               </CarouselItem>
@@ -172,8 +168,8 @@ export const Home: React.FC = () => {
           })}
         </Carousel>
         <S.ActionButtonsContainer>
-          <DislikeIcon />
-          <LikeIcon />
+          <DislikeIcon onClick={handleDislike} />
+          <LikeIcon onClick={handleLike} />
         </S.ActionButtonsContainer>
         <S.MissionNumberContainer>
           <SpyIcon />
